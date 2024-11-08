a9fd9394593d437c8abc997d735438069201f967
zeongii <a01051153088@gmail.com>
2024-11-08 18:51:14 +0900

    chat jenkins file 수정

--- ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
+++ ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
@@ -2,13 +2,10 @@ package com.bdool.chatservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = "spring.config.import=optional:configserver:")
 class ChatServiceApplicationTests {
     @Test
     void contextLoads() {
     }
 }
-
