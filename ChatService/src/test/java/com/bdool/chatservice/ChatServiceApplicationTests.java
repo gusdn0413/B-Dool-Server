8410d4eb904f2e74a6800bbf474de6ed36766c24
zeongii <a01051153088@gmail.com>
2024-11-08 18:22:09 +0900

    test 코드 삭제

--- ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
+++ ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
@@ -22,37 +22,9 @@ import java.util.UUID;
 @SpringBootTest
 class ChatServiceApplicationTests {
 
-    @Autowired
-    private MessageRepository messageRepository;
-
     @Test
     void contextLoads() {
 
-        List<MessageEntity> messages = new ArrayList<>();
-        try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\user\\Downloads\\Movie_Chat_Messages.csv"))) {
-            String[] nextLine;
-            reader.readNext(); // 헤더 스킵
-            while ((nextLine = reader.readNext()) != null) {
-                MessageEntity message = MessageEntity.builder()
-                        .messageId(UUID.fromString(nextLine[0])) // messageId
-                        .channelId(UUID.fromString(nextLine[1])) // channelId
-                        .content(nextLine[2]) // content
-                        .sendDate(LocalDateTime.parse(nextLine[3], DateTimeFormatter.ISO_DATE_TIME)) // sendDate
-                        .isEdited(Boolean.parseBoolean(nextLine[4])) // isEdited
-                        .isDeleted(Boolean.parseBoolean(nextLine[5])) // isDeleted
-                        .parentMessageId(nextLine[6].isEmpty() ? null : UUID.fromString(nextLine[6])) // parentMessageId
-//                        .profileId(UUID.fromString(nextLine[7])) // participantId
-                        .fileUrl(nextLine[8].isEmpty() ? null : nextLine[8]) // fileUrl
-                        .build();
-
-                messages.add(message);
-            }
-            // MongoDB에 데이터 저장
-            messageRepository.saveAll(messages);
-            System.out.println("CSV 데이터가 MongoDB에 성공적으로 삽입되었습니다.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
