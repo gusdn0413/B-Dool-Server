00ff87fedd164d9a136b4b2bbc977c0c5765a2ed
zeongii <a01051153088@gmail.com>
2024-11-08 18:30:44 +0900

    chat file 수정

--- ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
+++ ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
@@ -1,30 +1,14 @@
 package com.bdool.chatservice;
 
-import com.bdool.chatservice.model.Enum.SessionType;
-import com.bdool.chatservice.model.domain.SessionModel;
-import com.bdool.chatservice.model.entity.MessageEntity;
-import com.bdool.chatservice.model.repository.MessageRepository;
-import com.bdool.chatservice.service.MessageService;
-import com.bdool.chatservice.service.SessionService;
-import com.opencsv.CSVReader;
-import org.apache.kafka.common.protocol.Message;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.FileReader;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.import=optional:configserver:")
 class ChatServiceApplicationTests {
-
     @Test
     void contextLoads() {
-
     }
-
 }
+
